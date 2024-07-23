e7949c5ae0cff858fcd128b83d9af9d777c1ba0d
unknown <dannybooth>
2024-07-23 23:08:56 +0100

    components

---
+++ components/App/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import './App.css';
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([
+    { id: 1, name: "Track 1", artist: "Artist 1", album: "Album 1" },
+    { id: 2, name: "Track 2", artist: "Artist 2", album: "Album 2" },
+    { id: 3, name: "Track 3", artist: "Artist 3", album: "Album 3" }
+  ]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
+
+  const addTrack = (track) => {
+    if (playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    setPlaylistTracks([...playlistTracks, track]);
+  };
+
+  const removeTrack = (track) => {
+    setPlaylistTracks(playlistTracks.filter(currentTrack => currentTrack.id !== track.id));
+  };
+
+  const updatePlaylistName = (name) => {
+    setPlaylistName(name);
+  };
+
+  const savePlaylist = () => {
+    // Add Spotify saving logic here
+  };
+
+  const search = (term) => {
+    // Add Spotify search logic here
+  };
+
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar onSearch={search} />
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist
+            playlistName={playlistName}
+            playlistTracks={playlistTracks}
+            onRemove={removeTrack}
+            onNameChange={updatePlaylistName}
+            onSave={savePlaylist}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
