1d25520f1f8cf7dc53a7afccda519278422d3d23
unknown <dannybooth>
2024-07-25 23:58:51 +0100

    Remove ignored files from tracking

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App';
+import SearchBar from './components/SearchBar/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jammming</h1>
+      </header>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
